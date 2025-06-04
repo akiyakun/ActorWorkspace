159fa046be7fec4cbcdc7b3d4f12d9dd3a20ff35
akiya <akiya@MacBookPro.elecom>
2025-06-04 16:18:31 +0900

    SpineHelperクラス作成

---
+++ ActorWorkspace/Assets/SpineHelper/SpineHelper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public static class SpineHelper
+{
+#if UNITY_EDITOR
+    public static GameObject CreateSkeletonGameObjectFromAsset(string path)
+    {
+        var skeletonDataAsset = AssetDatabase.LoadAssetAtPath<SkeletonDataAsset>(path);
+        var newSkeleton = new GameObject(skeletonDataAsset.name);
+        var skeletonAnimation = newSkeleton.AddComponent<SkeletonAnimation>();
+        skeletonAnimation.skeletonDataAsset = skeletonDataAsset;
+        skeletonAnimation.Initialize(true);
+
+        // skeletonAnimation.skeleton.SetSkin(skins.Items[skinIndex]);
+        skeletonAnimation.Skeleton.SetSlotsToSetupPose();
+
+        return newSkeleton;
+    }
+#endif
+}
